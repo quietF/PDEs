fdd4bad318fd14be054149eea65bde2d7cfc115e
quietF <a.cathey.c@gmail.com>
2017-03-17 16:42:48 +0000

    good dynamics in CahnHiliard. started Poisson

--- src/CahnHiliard.java
+++ src/CahnHiliard.java
@@ -5,11 +5,11 @@ import java.io.UnsupportedEncodingException;
 public class CahnHiliard {
 	
 	private final double a=0.1, M=0.1, kappa=0.01, noise=0.01,
-			dx = 10., dt = 0.01;
+			dx = 0.5, dt = 0.1;
 	private int N=100;
 	private int[] iPlus = new int[N], iMinus = new int[N];
-	private double[][] phi = new double[N][N], mu = new double[N][N], f = new double[N][N];
-	private double[][] phiNew = new double[N][N], muNew = new double[N][N], fNew = new double[N][N] ;
+	private double[][] phi = new double[N][N], phiNew = new double[N][N],
+			mu = new double[N][N], f = new double[N][N];
 	
 	public CahnHiliard(int N, double phi0) {
 		this.N = N;
@@ -27,54 +27,52 @@ public class CahnHiliard {
 		iMinus[0]=N-1;
 	}
 	
-	private void setMu(){
-		mu = new double[N][N];
-		muNew = new double[N][N];
-		double phi_ij = 0., d2phi_ij=0.;
+	private void setMu_and_free(){
+		
+		
+		double phi_ij = 0., dphi_ij=0., dphi_i=0, dphi_j=0;
+		
 		for(int i=0; i<N; i++)
 			for(int j=0; j<N; j++){
 				phi_ij = phi[i][j];
-				d2phi_ij = phi[iPlus[i]][j]+phi[iMinus[i]][j]+phi[i][iPlus[j]]+
-						phi[i][iMinus[j]]-4*phi_ij;
-				d2phi_ij/=(dx*dx);
-				mu[i][j] = -a*phi_ij+a*phi_ij*phi_ij*phi_ij-kappa*d2phi_ij;
-			}
-	}
-	
-	private void setFree(){
-		f = new double[N][N];
-		fNew = new double[N][N];
-		double phi_ij = 0., dphi_ij=0.;
-		for(int i=0; i<N; i++)
-			for(int j=0; j<N; j++){
-				phi_ij = phi[i][j];
-				dphi_ij = phi[iPlus[i]][j]+phi[iMinus[i]][j]+phi[i][iPlus[j]]+phi[i][iMinus[j]]-4*phi_ij;
-				dphi_ij/=dx;
-				f[i][j] = -a*phi_ij*phi_ij/2.+a*phi_ij*phi_ij*phi_ij*phi_ij/4.+kappa*dphi_ij*dphi_ij/2.;
+				dphi_i = (phi[iPlus[i]][j]-phi[iMinus[i]][j])/(2.*dx);
+				dphi_j = (phi[i][iPlus[j]]-phi[i][iMinus[j]])/(2.*dx);
+				dphi_ij = phi[iPlus[i]][j]+phi[iMinus[i]][j]+phi[i][iPlus[j]]+
+						phi[i][iMinus[j]]-4.0*phi_ij;
+				dphi_ij/=(dx*dx);
+				mu[i][j] = -a*phi_ij+a*phi_ij*phi_ij*phi_ij-kappa*dphi_ij;
+				f[i][j] = -0.5*a*phi_ij*phi_ij+0.25*a*phi_ij*phi_ij*phi_ij*phi_ij+
+						0.5*kappa*(dphi_i*dphi_i+dphi_j*dphi_j);
 			}
 	}
 	
 	private void init(double phi0){
+		
 		phi = new double[N][N];
-		phiNew = new double[N][N];
+		mu = new double[N][N];
+		f = new double[N][N];
 		this.setPlusMinus();
+		
 		for(int i=0; i<N; i++)
 			for(int j=0; j<N; j++)
 				phi[i][j] = phi0 + (Math.random()-0.5)*noise;
-		this.setMu();
-		this.setFree();
+		
+		this.setMu_and_free();
 	}
 	
 	private void setNew(){
 		
+		phiNew = new double[N][N];
+		
 		for(int i=0; i<N; i++)
 			for(int j=0; j<N; j++){
-				phiNew[i][j] = phi[i][j] + M*dt*(mu[iPlus[i]][j]+mu[iMinus[i]][j]+mu[i][iPlus[j]]+
-						mu[i][iMinus[j]]-4*mu[i][j])/(dx*dx);
+				phiNew[i][j] = phi[i][j] + M*dt*(mu[iPlus[i]][j]+
+						mu[iMinus[i]][j]+mu[i][iPlus[j]]+
+						mu[i][iMinus[j]]-4.0*mu[i][j])/(dx*dx);
 			}
+		
 		phi = phiNew;
-		this.setMu();
-		this.setFree();
+		this.setMu_and_free();
 	}
 	
 	private double totalFree(){
@@ -85,20 +83,22 @@ public class CahnHiliard {
 		return free;
 	}
 	
-	public void evolve(String outFile, String freeFile, int dataPoints, int nt) 
+	public void evolve(String phiFile, String freeFile, int dataPoints, int nt) 
 			throws FileNotFoundException, UnsupportedEncodingException{
 		
 		PrintWriter writer2 = new PrintWriter(freeFile, "UTF-8");
 		for(int t=0; t<nt; t++){
 			if(t%dataPoints==0 && t!=0){
-				PrintWriter writer1 = new PrintWriter(outFile, "UTF-8");
-				writer1.printf(N + " ");
-				for(int j=0; j<N; j++) writer1.printf(j + " ");
-				writer1.println();
+				PrintWriter writer1 = new PrintWriter(phiFile, "UTF-8");
+				//writer1.printf(N + " ");
+				//for(int j=0; j<N; j++) writer1.printf(j + " ");
+				//writer1.println();
 				for(int i=0; i<N; i++){
-					writer1.printf(i + " ");
-					for(int j=0; j<N; j++)
-						writer1.printf(phi[i][j] + " ");
+					//writer1.printf(i + " ");
+					for(int j=0; j<N; j++){
+						//writer1.printf(phi[i][j] + " ");
+						writer1.println(i + " " + j + " " + phi[i][j]);
+					}
 					writer1.println();
 				}
 				writer1.close();
@@ -110,11 +110,14 @@ public class CahnHiliard {
 		
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+	public static void main(String[] args) 
+			throws FileNotFoundException, UnsupportedEncodingException {
 		System.out.println("HOLA");
-		CahnHiliard CH = new CahnHiliard(50, -0.5);
+		CahnHiliard CH = new CahnHiliard(100, 0.0);
 		int dataPoints = 1000;
-		CH.evolve("CHphiDensity.dat", "CHfreeEnergyDensity.dat", dataPoints, 10000000);
+		String outPhi = "CHphiDensity.dat", 
+				outFree = "CHfreeEnergyDensity.dat";
+		CH.evolve(outPhi, outFree, dataPoints, 10000000);
 	}
 	
 }
---
+++ src/Poisson.java
@@ -0,0 +1,94 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+public class Poisson {
+	
+	private final double eps=1., dx = 1., dt = 0.1;
+	private int N=100;
+	private int[] iPlus = new int[N], iMinus = new int[N];
+	private double[][][] phi = new double[N][N][N], phiNew = new double[N][N][N];
+	private double[][][] rho = new double[N][N][N];
+	private double[] E_ijk = new double[3];
+	
+	public Poisson(int N, double phi0) {
+		this.N = N;
+		this.init(phi0);
+	}
+	
+	private void setPlusMinus(){
+		iPlus = new int[N];
+		iMinus = new int[N];
+		for(int i=0; i<N; i++){
+			iPlus[i]=i+1;
+			iMinus[i]=i-1;
+		}
+		iPlus[N-1]=0;
+		iMinus[0]=N-1;
+	}
+	
+	private void init(double phi0){
+		
+		this.setPlusMinus();
+		phi = new double[N][N][N];
+		rho = new double[N][N][N];
+		
+		for(int i=0; i<N; i++)
+			for(int j=0; j<N; j++)
+				for(int k=0; k<N; k++){
+					if (i*j*k < 8) rho[i][j][k] = 1.0;
+					else rho[i][j][k] = 0.;
+					phi[i][j][k] = phi0;
+				}
+		
+	}
+	
+	private void getE_ijk(int i, int j, int k){
+		E_ijk[0] = -(phi[iPlus[i]][j][k]-phi[iMinus[i]][j][k])/(2.*dx); 
+		E_ijk[1] = -(phi[i][iPlus[j]][k]-phi[i][iMinus[j]][k])/(2.*dx); 
+		E_ijk[2] = -(phi[i][j][iPlus[k]]-phi[i][j][iMinus[k]])/(2.*dx);
+	}
+	
+	private void setNew(){
+		
+		phiNew = new double[N][N][N];
+		
+		for(int i=0; i<N; i++)
+			for(int j=0; j<N; j++)
+				for(int k=0; k<N; k++)
+					phiNew[i][j][k] = (phi[iPlus[i]][j][k]+phi[iMinus[i]][j][k]+
+							phi[i][iPlus[j]][k]+phi[i][iMinus[j]][k]+
+							phi[i][j][iPlus[k]]+phi[i][j][iMinus[k]])/6.0;
+		
+		phi = phiNew;
+	}
+	
+	public void evolve(String phiFile, String freeFile, int dataPoints, int nt) 
+			throws FileNotFoundException, UnsupportedEncodingException{
+		
+		for(int t=0; t<nt; t++){
+			if(t%dataPoints==0 && t!=0){
+				PrintWriter writer1 = new PrintWriter(phiFile, "UTF-8");
+				for(int i=0; i<N; i++){
+					for(int j=0; j<N; j++)
+						writer1.println(i + " " + j + " " + phi[i][j]);
+					writer1.println();
+				}
+				writer1.close();
+			}
+			this.setNew();
+		}
+		
+	}
+	
+	public static void main(String[] args) 
+			throws FileNotFoundException, UnsupportedEncodingException {
+		System.out.println("HOLA");
+		/*Poisson poiss = new Poisson(100, 0.0);
+		int dataPoints = 1000;
+		String outPhi = "CHphiDensity.dat", 
+				outFree = "CHfreeEnergyDensity.dat";
+		poiss.evolve(outPhi, outFree, dataPoints, 10000000);*/
+	}
+	
+}
