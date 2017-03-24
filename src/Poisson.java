8825a50e7fbebc5b5809e6c838d958aaa484f51f
quietF <a.cathey.c@gmail.com>
2017-03-24 00:04:09 +0000

    Poisson going better

--- src/Poisson.java
+++ src/Poisson.java
@@ -6,40 +6,41 @@ public class Poisson {
 	
 	private final double eps=1., dx = 1., dt = 0.1;
 	private int N=100;
-	private int[] iPlus = new int[N], iMinus = new int[N];
-	private double[][][] phi = new double[N][N][N], phiNew = new double[N][N][N];
-	private double[][][] rho = new double[N][N][N];
+	private int[] iPlus = new int[N+1], iMinus = new int[N+1];
+	private double[][][] phi = new double[N+1][N+1][N+1], 
+			phiNew = new double[N+1][N+1][N+1];
+	private double[][][] rho = new double[N+1][N+1][N+1];
 	private double[] E_ijk = new double[3];
 	
 	public Poisson(int N, double phi0) {
 		this.N = N;
-		this.init(phi0);
+		this.init();
 	}
 	
 	private void setPlusMinus(){
-		iPlus = new int[N];
-		iMinus = new int[N];
-		for(int i=0; i<N; i++){
+		iPlus = new int[N+1];
+		iMinus = new int[N+1];
+		for(int i=1; i<N; i++){
 			iPlus[i]=i+1;
 			iMinus[i]=i-1;
 		}
-		iPlus[N-1]=0;
-		iMinus[0]=N-1;
 	}
 	
-	private void init(double phi0){
+	private void init(){
 		
 		this.setPlusMinus();
-		phi = new double[N][N][N];
-		rho = new double[N][N][N];
+		phi = new double[N+1][N+1][N+1];
+		rho = new double[N+1][N+1][N+1];
 		
-		for(int i=0; i<N; i++)
-			for(int j=0; j<N; j++)
-				for(int k=0; k<N; k++){
-					if (i*j*k < 8) rho[i][j][k] = 1.0;
+		for(int i=1; i<N; i++)
+			for(int j=1; j<N; j++)
+				for(int k=1; k<N; k++){
+					if ((i-N/2)*(i-N/2)+(j-N/2)*(j-N/2)+
+							(k-N/2)*(k-N/2) < N) rho[i][j][k] = 1.0;
 					else rho[i][j][k] = 0.;
-					phi[i][j][k] = phi0;
 				}
+		
+		this.setNew();
 		
 	}
 	
@@ -51,14 +52,15 @@ public class Poisson {
 	
 	private void setNew(){
 		
-		phiNew = new double[N][N][N];
+		phiNew = new double[N+1][N+1][N+1];
 		
-		for(int i=0; i<N; i++)
-			for(int j=0; j<N; j++)
-				for(int k=0; k<N; k++)
+		for(int i=1; i<N; i++)
+			for(int j=1; j<N; j++)
+				for(int k=1; k<N; k++)
 					phiNew[i][j][k] = (phi[iPlus[i]][j][k]+phi[iMinus[i]][j][k]+
 							phi[i][iPlus[j]][k]+phi[i][iMinus[j]][k]+
-							phi[i][j][iPlus[k]]+phi[i][j][iMinus[k]])/6.0;
+							phi[i][j][iPlus[k]]+phi[i][j][iMinus[k]]+
+							rho[i][j][k]/eps)/6.0;
 		
 		phi = phiNew;
 	}
@@ -66,12 +68,12 @@ public class Poisson {
 	public void evolve(String phiFile) 
 			throws FileNotFoundException, UnsupportedEncodingException{
 		
-		//for(int t=0; t<nt; t++){
-			//if(t%dataPoints==0 && t!=0){
+		for(int t=0; t<10000000; t++){
+			if(t%10000==0 && t!=0){
 				PrintWriter writer1 = new PrintWriter(phiFile, "UTF-8");
-				for(int i=0; i<N; i++){
-					for(int j=0; j<N; j++)
-						for(int k=0; k<N; k++){
+				for(int i=1; i<N; i++){
+					for(int j=1; j<N; j++)
+						for(int k=1; k<N; k++){
 							this.getE_ijk(i, j, k);
 							writer1.println(i + " " + j + " " + k +  " " + 
 									rho[i][j][k] + " " + phi[i][j][k] + " " +
@@ -80,22 +82,17 @@ public class Poisson {
 					writer1.println();
 				}
 				writer1.close();
-			//}
-			//this.setNew();
-		//}
+			}
+			this.setNew();
+		}
 		
 	}
 	
 	public static void main(String[] args) 
 			throws FileNotFoundException, UnsupportedEncodingException {
 		System.out.println("HOLA");
-		Poisson poiss = new Poisson(100, 0.0);
+		Poisson poiss = new Poisson(50, 0.0);
 		poiss.evolve("Poisson.dat");
-		/*Poisson poiss = new Poisson(100, 0.0);
-		int dataPoints = 1000;
-		String outPhi = "CHphiDensity.dat", 
-				outFree = "CHfreeEnergyDensity.dat";
-		poiss.evolve(outPhi, outFree, dataPoints, 10000000);*/
 	}
 	
 }
